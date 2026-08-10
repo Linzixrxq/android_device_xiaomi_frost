9e222cedf9a6d5f339f406a204896613911898d7
Linzixrxq <sunats460@gmail.com>
2026-08-11 01:28:41 +0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,53 +6,24 @@
 
 LOCAL_PATH := device/xiaomi/frost
 
-# Platform Information 
-PRODUCT_PLATFORM := frost
-
-# A/B
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-# Boot control HAL
+# Soong namespaces (указываем пути к вендорным модулям JLQ)
 PRODUCT_SOONG_NAMESPACES += \
-    vendor/xiaomi/frost \
-    hardware/jlq/jr510 \
+    device/xiaomi/frost \
+    hardware/jlq \
     vendor/jlq/jr510
-   
+
+# A/B Boot Control HAL (для работы со слотами A и B на Virtual A/B)
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service \
-    android.hardware.boot@1.0-impl-wrapper \
-    bootctrl.$(PRODUCT_PLATFORM)
-
-PRODUCT_PACKAGES += \
     bootctrl.jr510
 
-PRODUCT_BOOT_CONTROL_HAL := \
-    bootctrl.jr510 \
-    libgptutils \
-    libz \
-    libcutils
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
-    
-# Use Sdcardfs
+# Свойства флеш-памяти и накопителей
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.sys.sdcardfs=1
 
-# --- ПОДКЛЮЧЕНИЕ TWRP (ДОБАВЛЕНО) ---
-# Включаем базовые пакеты TWRP в сборку
+# Включаем сам TWRP и подключаем общий конфиг TeamWin
 PRODUCT_PACKAGES += \
     twrp
 
-# Наследуем конфигурацию интерфейса и утилит TeamWin
 $(call inherit-product, vendor/twrp/config/common.mk)
-
