f3ab771c9ce01a9b749cfedb7509225eb4dd3c96
Linzixrxq <sunats460@gmail.com>
2026-08-11 01:30:17 +0500

    Update twrp_frost.mk

--- twrp_frost.mk
+++ twrp_frost.mk
@@ -1,8 +1,8 @@
 # Наследование от базовой конфигурации AOSP / TWRP для архитектуры ARM64
-#$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Наследование настроек самого устройства, которые мы правили в device.mk
+# Наследование настроек устройства
 $(call inherit-product, device/xiaomi/frost/device.mk)
 
 # Официальные параметры идентификации смартфона POCO C40
@@ -12,5 +12,5 @@ PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO C40
 PRODUCT_MANUFACTURER := Xiaomi
 
-# Строка, которая будет отображаться в логах при сборке
+# Идентификация сборки
 PRODUCT_RELEASE_NAME := frost
