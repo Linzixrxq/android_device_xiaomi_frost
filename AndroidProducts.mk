8cb81fb2d953c9a0f256c9d699450880dab7d53c
Linzixrxq <sunats460@gmail.com>
2026-08-15 13:27:16 +0500

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_frost.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_frost-user \
+    twrp_frost-userdebug \
+    twrp_frost-eng
---
+++ device.mk
@@ -0,0 +1,33 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/xiaomi/frost
+
+# Soong namespaces (указываем пути к вендорным модулям JLQ)
+PRODUCT_SOONG_NAMESPACES += \
+    device/xiaomi/frost \
+    hardware/jlq \
+    vendor/jlq/jr510
+
+# A/B Boot Control HAL (для работы со слотами A и B на Virtual A/B)
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service \
+    bootctrl.jr510
+
+# Свойства флеш-памяти и накопителей
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sys.sdcardfs=1
+
+# Включаем сам TWRP и подключаем общий конфиг TeamWin
+PRODUCT_PACKAGES += \
+    twrp
+
+# Копирование fstab в vendor_ramdisk для first_stage_mount
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fstab.jlq:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_mount.fstab
+
+$(call inherit-product, vendor/twrp/config/common.mk)
