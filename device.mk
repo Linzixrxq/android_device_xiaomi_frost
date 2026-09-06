d0a9beb868f06598403e9a1eb19a0303c9a5f10a
Linzixrxq <sunats460@gmail.com>
2026-09-06 17:55:25 +0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,33 +1,15 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 LOCAL_PATH := device/xiaomi/frost
 
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += \
-    device/xiaomi/frost
-
-# A/B Boot Control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service \
-    bootctrl.jr510
-
-# Свойства флеш-памяти и накопителей
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.sys.sdcardfs=1
-
-# Включаем TWRP
-PRODUCT_PACKAGES += \
-    twrp
-
-# Копирование fstab и dtb
+# Копирование recovery.fstab в рамдиск рекавери
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/fstab.jlq:root/first_stage_mount.fstab \
-    $(LOCAL_PATH)/fstab.jlq:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/recovery.fstab \
-    $(LOCAL_PATH)/prebuilt/dtb.img:dtb.img
+    $(LOCAL_PATH)/recovery.fstab:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/recovery.fstab
 
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Базовые свойства экрана и графики
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=320 \
+    ro.hardware.egl=mali \
+    ro.hardware.vulkan=pastel
+
+# Дополнительные модули
+PRODUCT_PACKAGES += \
+    qcom_dec_health
