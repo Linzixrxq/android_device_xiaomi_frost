8086df247103b598285720b06587687c6353a04c
Linzixrxq <sunats460@gmail.com>
2026-09-06 17:58:50 +0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,15 +1,18 @@
 LOCAL_PATH := device/xiaomi/frost
 
-# Копирование recovery.fstab в рамдиск рекавери
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery.fstab:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/recovery.fstab
+# Архитектура виртуального A/B и динамических разделов
+PRODUCT_VIRTUAL_AB_OTA := true
 
-# Базовые свойства экрана и графики
+# Копирование стокового fstab.jlq в путь, необходимый для TWRP
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fstab.jlq:$(TARGET_COPY_OUT_RECOVERY)/root/system/etc/recovery.fstab
+
+# Настройки экрана и графического процессора Mali
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=320 \
     ro.hardware.egl=mali \
     ro.hardware.vulkan=pastel
 
-# Дополнительные модули
+# Дополнительные пакеты рекавери
 PRODUCT_PACKAGES += \
     qcom_dec_health
