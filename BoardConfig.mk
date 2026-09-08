555691795714b3a1ff826e47fc2b508dd3a0c979
Linzixrxq <sunats460@gmail.com>
2026-09-08 22:29:55 +0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,66 +1,68 @@
 DEVICE_PATH := device/xiaomi/frost
 
-# Архитектура и платформа (JLQ JR510 / ARM64)
+# Включаем минимальный манифест
+ALLOW_MISSING_DEPENDENCIES := true
+
+# Платформа и процессор
+TARGET_BOARD_PLATFORM := jr510
+TARGET_BOOTLOADER_BOARD_NAME := frost
+
+# Архитектура (ARM64 / Cortex-A55)
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a55
+TARGET_CPU_VARIANT := generic
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a55
+TARGET_2ND_CPU_VARIANT := generic
 
-TARGET_BOARD_PLATFORM := jr510
-
-# Обязательные флаги структуры Recovery-in-Boot
-BOARD_USES_RECOVERY_AS_BOOT := true
+# Важнейшие флаги структуры Recovery-in-Boot
 TARGET_NO_RECOVERY := true
+BOARD_USES_RECOVERY_AS_BOOT := true
+AB_OTA_UPDATER := true
 
-# Исправлено на Boot Header v2 (Чтобы упаковать Ramdisk TWRP прямо внутрь boot.img)
-BOARD_BOOT_HEADER_VERSION := 2
-BOARD_PAGE_SIZE := 2048
-
-# Предкомпилированное ядро и DTB
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-
-# Смещения (Скорректированы под Header v2)
-BOARD_KERNEL_OFFSET := 0x00008000
-BOARD_RAMDISK_OFFSET := 0x01000000
-BOARD_KERNEL_TAGS_OFFSET := 0x00000000
-BOARD_DTB_OFFSET := 0x00000000
-
-# Командная строка ядра
-BOARD_KERNEL_CMDLINE := security=selinux androidboot.hardware=jlq iommu.strict=0 firmware_class.path=/etc/firmware swiotlb=2048 rcu_nocbs=0-7 kpti=off rcupdate.rcu_expedited=1 earlycon=uart8250,mmio32,0x3450F000 console=jlqttyS1,115200n8 no_console_suspend loglevel=4 buildvariant=user androidboot.selinux=permissive
-
-# Формат сжатия ramdisk, который требует заводской загрузчик
-BOARD_RAMDISK_USE_LZ4 := true
-
-# Аргументы для mkbootimg
+# Конфигурация bootimg, подтвержденная стоком лога
+BOARD_BOOT_HEADER_VERSION := 3
+BOARD_PAGE_SIZE := 4096
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_PAGE_SIZE)
-BOARD_MKBOOTIMG_ARGS += --kernel_offset $(BOARD_KERNEL_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
-BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
 
-# Динамические разделы
-BOARD_SUPER_PARTITION_GROUPS := jlq_dynamic_partitions
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
-BOARD_HAS_NO_SELECT_BUTTON := true
+# Предкомпилированное ядро
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+BOARD_RAMDISK_USE_LZ4 := true
 
-# Реальный максимальный размер раздела boot на Poco C40
-BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+# Реальный точный размер раздела boot на Poco C40 (96MB)
+BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296
+BOARD_AVB_ENABLE := true
 
-# Настройки TWRP (Временно отключена крипта для стабильности запуска на GSI)
+# Флаги совместимости с разметкой
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_USES_METADATA_PARTITION := true
+TARGET_COPY_OUT_VENDOR := vendor
+BOARD_ROOT_EXTRA_FOLDERS := cust
+
+# Графика дисплея Poco C40 (Чтобы экран не вис на логотипе)
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+TARGET_SCREEN_DENSITY := 320
 TW_THEME := portrait_hdpi
+TW_BRIGHTNESS_PATH := /sys/class/backlight/panel0-backlight/brightness
+TW_DEFAULT_BRIGHTNESS := 1800
+TW_MAX_BRIGHTNESS := 3514
+TW_NO_SCREEN_BLANK := true
+
+# Базовые параметры TWRP
+TW_DEFAULT_LANGUAGE := ru
 TW_EXTRA_LANGUAGES := true
-TW_SCREEN_BLANK_ON_BOOT := true
-INPUT_EVENT_LOGGING := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_INCLUDE_CRYPTO := false
 TW_USE_TOOLBOX := true
+TW_NO_HAPTICS := true
+TW_EXCLUDE_APEX := true
+
+# Отключаем крипту для первого успешного теста на Android 13 GSI
+TW_INCLUDE_CRYPTO := false
+
+# Фикс для сборки
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
